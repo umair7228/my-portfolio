c3cbcab63164ee829799ba752ecb374b5bd04108
umair7228 <umairnawaz7228@gmail.com>
2024-12-03 12:17:29 +0500

    Projects Updated

--- src/app/data.js
+++ src/app/data.js
@@ -41,35 +41,35 @@ export const projectSections = {
 
 export const dataEngineeringProjects = {
   COVIDDataPipeline: {
-    id: 10,
+    id: 15,
     name: "Pandemic Insights: ETL Pipeline with AWS Glue, Athena & Redshift",
     description:
       "Developed a scalable ETL pipeline using AWS Glue, Athena, and Redshift to process and analyze COVID-19 data from multiple sources. Designed fact and dimension tables, optimized data queries, and loaded the results into Redshift for data-driven insights.",
     demoLink: "https://github.com/umair7228/COVID-Data-Pipeline",
   },
   serverlessDataLake: {
-    id: 10,
+    id: 14,
     name: "Serverless Data Lake Architecture on AWS",
     description:
       "A fully automated serverless data lake solution using AWS services to ingest, process, and notify users about data transformations. This architecture leverages S3, Lambda, Glue Crawlers and Jobs, CloudWatch, and SNS to streamline ETL processes and deliver scalable data insights.",
     demoLink: "https://github.com/umair7228/Serverless-Data-Lake-Architecture",
   },
   dataflowInsights: {
-    id: 10,
+    id: 13,
     name: "Dataflow Insights",
     description:
       "DataFlow Insights is an automated data pipeline project that pushes daily data to Amazon S3, uses AWS Glue to catalog it, and Amazon Athena to query it. Finally, the data is visualized in Amazon QuickSight, creating a streamlined process for insightful data analysis and visualization.",
     demoLink: "https://github.com/umair7228/DataFlow-Insights",
   },
   eventDrivenArchitecture: {
-    id: 10,
+    id: 12,
     name: "Event-Driven Data Pipeline with AWS",
     description:
       "Built a real-time data pipeline using AWS services including (S3, SNS, SQS, Lambda) to process events asynchronously, ensuring reliability, scalability, and fault tolerance. The architecture demonstrates an event-driven approach, where events in an S3 bucket trigger a flow through SNS, SQS, and Lambda, with the processed output stored back in S3.",
     demoLink: "https://github.com/umair7228/event-driven-architecture",
   },
   stockMarketPipeline: {
-    id: 9,
+    id: 11,
     name: "Real-Time Stock Market Data Pipeline",
     description:
       "Built a real-time data pipeline for stock market data. It integrates Apache Kafka for data streaming and multiple AWS services for data storage and querying.",
@@ -81,14 +81,14 @@ export const dataEngineeringProjects = {
     name: "Weather and S3 Data Integration Pipeline",
     description:
       "Developed a data pipeline using Apache Airflow on AWS EC2 to integrate weather data from OpenWeather API and Amazon S3 into an RDS PostgreSQL database. Transformed and joined data in parallel, with the final output stored in Amazon S3.",
-    demoLink: "https://github.com/umair7228/Real-Estate-Data-Pipeline",
+    demoLink: "https://github.com/umair7228/parallel-etl-pipeline",
   },
   scdImplementationProject: {
     id: 9,
     name: "Real-Time Data Pipeline with SCD Implementation",
     description:
       "Developed an end-to-end data pipeline that generates synthetic data using Python, extracts and transfers it via NiFi to S3, and ingests it into Snowflake using Snowpipe, with SCD Type 1 & 2 implementations for effective data tracking and management.",
-    demoLink: "https://github.com/umair7228/Real-Estate-Data-Pipeline",
+    demoLink: "https://github.com/umair7228/Real-Time-Data-Pipeline-with-AWS-NiFi-and-Snowflake",
   },
   redfinProject: {
     id: 8,
@@ -158,21 +158,21 @@ export const frontendProjects = {
     name: "Responsive & Animated Portfolio Website",
     description:
       "Developed a fully responsive and animated portfolio website to showcase projects, skills, and experience. This visually engaging site features smooth animations, easy navigation, and a user-friendly interface, enhancing both accessibility and aesthetic appeal.",
-    demoLink: "https://umair-portfolio-web.vercel.app/",
+    demoLink: "https://github.com/umair7228/my-portfolio",
   },
   schoolProject: {
     id: 10,
     name: "School Academy Website",
     description:
       "Developed a responsive website for an educational academy with Next.js, Tailwind CSS, and React.js. The design emphasizes easy navigation, clean visuals, and accessibility for students and educators.",
-    demoLink: "https://umair-school-academy.vercel.app/",
+    demoLink: "https://github.com/umair7228/school-web-css",
   },
   techAgencyProject: {
     id: 9,
     name: "Tech Agency Website",
     description:
       "Created a responsive, professional tech agency site using Next.js, Tailwind CSS, and React.js, featuring an intuitive layout to showcase services and projects.",
-    demoLink: "https://umair-tech-agency.vercel.app/",
+    demoLink: "https://github.com/umair7228/tech-agency-web",
   },
   dashboardProject: {
     id: 8,
